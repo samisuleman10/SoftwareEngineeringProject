0307f89ba6a30253d19f56b2be2ff06737dec6d8
Shifa Masood <shifamasood2@gmail.com>
2020-12-07 17:34:46 +0100

    Created UML Diagrams (UseCase and Sequence Diagram)

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
