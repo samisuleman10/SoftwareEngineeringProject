d8e35689309cbac628a3c267d7146349fc44917b
Shifa Masood <shifamasood2@gmail.com>
2020-12-24 12:59:21 +0100

    Changed class structure and improved game functionality

--- main.py
+++ main.py
@@ -16,13 +16,95 @@ class GameBoard:
 
     def __init__(self):
         self.menu()
-        self.username = ""
 
     def menu(self):
         print("\nWelcome to the word guessing game\n")
         print("1. Start game\n2. About game\n3. Exit")
         selected_option = input("Enter key:")
         self.display_selected_option(selected_option)
+
+    def about_game(self):
+        print(
+            "A word guessing game in which the player is told how many letters are in the word.\n"
+            + "The game also allows user to select difficulty level and different categories.\n"
+            + "The player must discover the word by guessing letters one at a time.\n"
+            + "Each correctly guessed letter is added to the word. On each wrong guess, a body part\n"
+            + "is added to a picture of a hanged man. Once the hanged man is complete, the player "
+            + "loses the game.")
+        menu_key = input("\nPress Enter to return to menu.")
+        if menu_key == '':
+            self.menu()
+        else:
+            print("please enter the correct key.\n")
+            self.about_game()
+
+    @staticmethod
+    def exit_game():
+        exit()
+
+    def display_selected_option(self, option):
+
+        if option == "1":
+            game_engine_obj.get_user_name()
+            game_engine_obj.difficulty_selection()
+        elif option == "2":
+            self.about_game()
+        elif option == "3":
+            self.exit_game()
+        else:
+            print("Please enter the correct key")
+            self.menu()
+
+
+class WordManagement:
+
+    def __init__(self):
+        self.secret_word = ""
+        self.game_progress = ""
+        self.category = ""
+
+    @staticmethod
+    def import_word_list(file):
+        with open(file, 'r') as f:
+            words = list(filter(None, f.read().split('\n')))
+            return words
+
+    def word_selection(self, word_category):
+        self.category = word_category
+        file_path = ""
+        if word_category == "1":
+            file_path = "Database/countries_cities.txt"
+        elif word_category == "2":
+            file_path = "Database/fruits_vegetables.txt"
+        else:
+            file_path = "Database/animals.txt"
+
+        words = self.import_word_list(file_path)
+        self.secret_word = self.get_random_word(words).lower()
+        self.game_progress = list('_' * len(self.secret_word))
+        game_engine_obj.play(self.secret_word, self.game_progress)
+
+    @staticmethod
+    def get_random_word(wordlist):
+        word_index = random.randint(0, len(wordlist) - 1)
+        return wordlist[word_index]
+
+
+class GameEngine:
+    def __init__(self):
+        self.failed_attempts = 0
+        self.total_attempts = 0
+        self.game_started = False
+        self.username = ""
+        self.difficulty = ""
+        self.game_progress = ""
+        self.secret_word = ""
+        self.word_category = ""
+        self.word_management_obj = WordManagement()
+
+    @staticmethod
+    def play_game():
+        GameBoard()
 
     def get_user_name(self):
         self.username = input('\nEnter your name: ')
@@ -39,79 +121,17 @@ class GameBoard:
         else:
             self.category_selection(difficulty_level)
 
-    def about_game(self):
-        print(
-            "A word guessing game in which the player is told how many letters are in the word.\n"
-            + "The game also allows user to select difficulty level and different categories.\n"
-            + "The player must discover the word by guessing letters one at a time.\n"
-            + "Each correctly guessed letter is added to the word. On each wrong guess, a body part\n"
-            + "is added to a picture of a hanged man. Once the hanged man is complete, the player "
-            + "loses the game.")
-
-    def exit_game(self):
-        exit()
-
     def category_selection(self, difficulty_level):
+        self.difficulty = difficulty_level
         print("\nPlease select the category\n")
         category_list = ["1", "2", "3"]
         print("1. Countries and Cities\n2. Fruits and Vegetables\n3. Animals")
-        word_category = input("Enter key:")
-        if word_category not in category_list:
+        self.word_category = input("Enter key:")
+        if self.word_category not in category_list:
             print("Please enter the correct key")
-            self.category_selection()
+            self.category_selection(self.difficulty)
         else:
-            game_engine_obj = GameEngine()
-            GameEngine.word_selection(game_engine_obj, self.username, word_category, difficulty_level)
-
-    def display_selected_option(self, option):
-
-        if option == "1":
-            self.get_user_name()
-            self.difficulty_selection()
-        elif option == "2":
-            self.about_game()
-        elif option == "3":
-            self.exit_game()
-        else:
-            print("Please enter the correct key")
-            self.menu()
-
-
-class GameEngine:
-
-    def __init__(self):
-        self.failed_attempts = 0
-        self.secret_word = ""
-        self.game_progress = ""
-        self.category = ""
-        self.difficulty = ""
-        self.username = ""
-        self.total_attempts = 0
-        self.game_started = False
-
-    def import_word_list(self, file):
-
-        with open(file, 'r') as f:
-            words = list(filter(None, f.read().split('\n')))
-            return words
-
-    def word_selection(self, user_name, word_category, difficulty_level):
-        self.category = word_category
-        self.difficulty = difficulty_level
-        self.username = user_name
-        file_path = ""
-
-        if word_category == "1":
-            file_path = "Database/countries_cities.txt"
-        elif word_category == "2":
-            file_path = "Database/fruits_vegetables.txt"
-        else:
-            file_path = "Database/animals.txt"
-
-        words = self.import_word_list(file_path)
-        self.secret_word = self.get_random_word(words).lower()
-        self.game_progress = list('_' * len(self.secret_word))
-        self.play()
+            self.word_management_obj.word_selection(self.word_category)
 
     def print_game_status(self):
         print('\n')
@@ -123,27 +143,28 @@ class GameEngine:
         for index in indexes:
             self.game_progress[index] = letter
 
-    def get_user_input(self):
+    @staticmethod
+    def get_user_input():
         user_input = input('\nPlease type a letter: ')
         return user_input
 
     def find_indexes(self, letter):
         return [i for i, char in enumerate(self.secret_word) if letter == char]
 
-    def get_random_word(self, wordlist):
-        word_index = random.randint(0, len(wordlist) - 1)
-        return wordlist[word_index]
-
     def is_invalid_letter(self, input_):
         return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
 
-    def playagain(self):
+    def play_again(self):
         print('Do you want to play again? (y/n)')
-        if input('> ').lower().startswith('y'):
-            self.word_selection(self.category, self.difficulty)
+        input_key = input('> ').lower()
+        if input_key == 'y':
+            self.failed_attempts = 0
+            self.word_management_obj.word_selection(self.word_category)
+        elif input_key == 'n':
+            self.play_game()
         else:
-            game_board_obj = GameBoard()
-            GameBoard.menu(game_board_obj)
+            print("Please enter the correct key!")
+            self.play_again()
 
     def get_total_attempts(self):
 
@@ -171,8 +192,10 @@ class GameEngine:
                 self.failed_attempts += 3
             self.game_started = True
 
-    def play(self):
+    def play(self, secret_word, game_progress):
 
+        self.secret_word = secret_word
+        self.game_progress = game_progress
         score_obj = ScoreManagement()
         self.get_total_attempts()
 
@@ -198,7 +221,7 @@ class GameEngine:
                     print('The word is: {0}'.format(self.secret_word))
                     ScoreManagement.updating_scores(score_obj, self.username, self.failed_attempts)
                     ScoreManagement.highest_scores(score_obj)
-                    self.playagain()
+                    self.play_again()
             else:
                 self.get_failed_attempts()
                 self.total_attempts -= 1
@@ -213,7 +236,7 @@ class GameEngine:
         print("\nSorry,You lost!")
 
         ScoreManagement.highest_scores(score_obj)
-        self.playagain()
+        self.play_again()
 
 
 class ScoreManagement:
@@ -247,9 +270,11 @@ class ScoreManagement:
             sorted_dict[w] = results[w]
 
         print("\nHighest Scores\n")
-        for name in sorted_dict[0:5]:
+        for name in sorted_dict:
             print(name, sorted_dict[name])
 
 
+game_engine_obj = GameEngine()
+
 if __name__ == '__main__':
-    GameBoard()
+    game_engine_obj.play_game()
